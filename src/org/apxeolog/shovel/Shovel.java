cc4a6998382b79ddacaf9ead973ea1e97fbe04a4
CarryGun <res@sibnet.ru>
2015-10-10 17:11:58 +0600

    Bump minor version.

--- src/org/apxeolog/shovel/Shovel.java
+++ src/org/apxeolog/shovel/Shovel.java
@@ -20,7 +20,7 @@ import java.nio.file.StandardOpenOption;
  * Main class to init startup data like configs etc
  */
 public class Shovel {
-    private static String version = "1.4.4";
+    private static String version = "1.4.5";
     private static Settings settings;
     private static File workingDirectory;
     private static File userDirectory;
